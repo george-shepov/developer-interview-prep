705155bbe3ed1782184d16907201a8d26cc31322
Giorgiy Shepov <40531545+george-shepov@users.noreply.github.com>
2026-06-10 18:10:36 -0400

    Add interview data loader

---
+++ data.js
@@ -0,0 +1,5 @@
+window.INTERVIEW_DATA = [];
+document.write('<script src="data-1.js"><\/script>');
+document.write('<script src="data-2.js"><\/script>');
+document.write('<script src="data-3.js"><\/script>');
+document.write('<script src="data-4.js"><\/script>');
