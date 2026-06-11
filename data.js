96914b63bf1e6dcb3519a9f851ef04d8b3cfa7f4
Giorgiy Shepov <40531545+george-shepov@users.noreply.github.com>
2026-06-10 20:44:12 -0400

    Start built-in deck with behavioral questions

--- data.js
+++ data.js
@@ -3,3 +3,4 @@ document.write('<script src="data-1.js"><\/script>');
 document.write('<script src="data-2.js"><\/script>');
 document.write('<script src="data-3.js"><\/script>');
 document.write('<script src="data-4.js"><\/script>');
+document.write('<script>window.INTERVIEW_DATA.sort(function(a,b){var ao=a.category==="Behavioral"?0:1;var bo=b.category==="Behavioral"?0:1;return ao-bo;});<\/script>');
